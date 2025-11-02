ef7a00a24a2dbc33d92caaf3a108bf29c6d81993
tsengexplore <tsengexplore@gmail.com>
2025-11-03 00:15:12 +0800

    Update App.vue

--- assets/App.vue
+++ assets/App.vue
@@ -596,8 +596,8 @@ export default {
           window.history.pushState(null, "", url.toString());
         }
         document.title = this.cwd.replace(/.*\/(?!$)|\//g, "") === "/" 
-            ? "FlareDrive-R2 - 优雅的 Cloudflare R2 网盘文件库"
-            :`${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/" } - 优雅的 Cloudflare R2 网盘文件库`;
+            ? "小舅子"
+            :`${this.cwd.replace(/.*\/(?!$)|\//g, "") || "/" } - 我的网盘文件库`;
       },
       immediate: true,
     },
